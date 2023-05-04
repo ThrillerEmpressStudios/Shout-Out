ea0ebb3bf7846d35413b17cd35d2f5e42591f459
The Luke <100490693+Luki013@users.noreply.github.com>
2023-05-04 19:34:05 -0300

    Improved menu and a credits scene

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,21 +1,47 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public class MainMenu : Control
 {
+    private AudioStream clickSound;
+    private PackedScene creditsScene;
+
     public override void _Ready()
     {
-        OS.WindowResizable = false;
+        creditsScene = GD.Load<PackedScene>("res://Scenes/Credits.tscn");
+
+        clickSound = GD.Load<AudioStream>("res://Assets/SFX/click_2.wav");
+
+        //OS.WindowResizable = false;
         OS.SetWindowTitle("");
     }
 
-    public void _on_StartGameButton_pressed()
+    public async void _on_StartGameButton_pressed()
     {
+        ClickButtonSound();
+        await Task.Delay(TimeSpan.FromSeconds(0.5));
         GetTree().ChangeScene("res://Scenes/Game.tscn");
+    }
+
+    public void _on_CreditsButton_pressed()
+    {
+        ClickButtonSound();
+        var creditsWindow = (Popup)creditsScene.Instance();
+        AddChild(creditsWindow);
+        creditsWindow.PopupCentered();
     }
 
     public void _on_QuitGame_pressed()
     {
+        ClickButtonSound();
         GetTree().Quit();
+    }
+
+    private void ClickButtonSound()
+    {
+        var clickAudio = GetNode<AudioStreamPlayer>("ClickButton");
+        clickAudio.Stream = clickSound;
+        clickAudio.Play();
     }
 }
